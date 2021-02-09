ab06b924e436d109ea1e801db19c013eabd78955
Lisa Chowdhury <47085691+lisa1704@users.noreply.github.com>
2021-02-10 01:10:36 +0600

    transportScreen done

--- Travel_App/src/screeens/TransportScreen.js
+++ Travel_App/src/screeens/TransportScreen.js
@@ -3,14 +3,61 @@ import { View, Text, StyleSheet, Alert, Modal, Image, ImageBackground } from "re
 import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
-const TransportScreen=()=>{
-    return(
+const TransportScreen = () => {
+    return (
+        <View style={styles.viewStyle}>
+        <Text style={styles.header}>Transport Sytem</Text>
         <TouchableOpacity>
-            <ImageBackground>
-                <Text>TAke a flight</Text>
+            <ImageBackground
+                source={require("../../assets/images/plane.jpg")}
+                style={styles.imgBackground}
+            >
+                <Text style={styles.textStyle}>Airplane</Text>
             </ImageBackground>
         </TouchableOpacity>
+        <TouchableOpacity>
+            <ImageBackground
+                source={require("../../assets/images/train2.jpg")}
+                style={styles.imgBackground}
+            >
+                <Text style={styles.textStyle}>Train</Text>
+            </ImageBackground>
+        </TouchableOpacity>
+        <TouchableOpacity>
+            <ImageBackground
+                source={require("../../assets/images/bus.jpg")}
+                style={styles.imgBackground}
+            >
+                <Text style={styles.textStyle}>Bus</Text>
+            </ImageBackground>
+        </TouchableOpacity>
+        </View>
     );
 };
+
+const styles = StyleSheet.create(
+    {
+        imgBackground: {
+            width: 450,
+            height: 200,
+            marginTop:30
+        },
+        textStyle:{
+            fontSize:25,
+            color:"black",
+            fontWeight:"bold",
+            paddingTop:90,
+            paddingLeft:10
+        },
+        viewStyle:{
+            paddingTop:100
+        },
+        header:{
+            fontSize:35,
+            fontWeight:"bold",
+            alignSelf:"center"
+        }
+    }
+)
 
 export default TransportScreen;
